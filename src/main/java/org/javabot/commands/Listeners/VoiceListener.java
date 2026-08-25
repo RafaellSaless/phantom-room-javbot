2abfe67dad2470ddf1a3bd5a006a5c603798434e
rafaellsaless <rafaelsales333.081@gmail.com>
2026-08-24 23:41:37 -0300

    feat: implement temporary voice channels

--- src/main/java/org/javabot/commands/Listeners/VoiceListener.java
+++ src/main/java/org/javabot/commands/Listeners/VoiceListener.java
@@ -2,39 +2,45 @@ package org.javabot.commands.Listeners;
 
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.javabot.commands.Managers.TempVoiceManager;
 import org.jetbrains.annotations.NotNull;
-
+import org.javabot.commands.Managers.ConfigManager;
+import org.javabot.commands.Managers.TempVoiceManager;
 
 public class VoiceListener extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
+
         if (event.getChannelJoined() != null) {
 
             String guildId = event.getGuild().getId();
             long canalEntradoId = event.getChannelJoined().getIdLong();
 
-            // Busca direto o canal configurado para este servidor no JSON
-            String canalConfiguradoId = org.javabot.commands.Managers.ConfigManager.getCanalDoServidor(guildId);
+            String canalConfiguradoId =
+                    ConfigManager.getCanalDoServidor(guildId);
 
-            if (canalConfiguradoId != null && canalConfiguradoId.equals(String.valueOf(canalEntradoId))) {
-                String nomeUsuario = event.getMember().getUser().getName();
-                System.out.println(nomeUsuario + " entrou no canal de criação de call do servidor " + guildId + "!");
+            if (canalConfiguradoId != null &&
+                    canalConfiguradoId.equals(String.valueOf(canalEntradoId))) {
+
+                System.out.println(
+                        event.getMember().getEffectiveName()
+                                + " entrou no canal de criação de call."
+                );
 
                 TempVoiceManager.criarCall(event);
             }
         }
-                if (event.getChannelLeft() != null) {
 
-                    long canalSaidoId = event.getChannelLeft().getIdLong();
+        if (event.getChannelLeft() != null) {
 
-                    TempVoiceManager.verificarCallVazia(
-                            event.getGuild(),
-                            canalSaidoId
-                    );
+            long canalSaidoId =
+                    event.getChannelLeft().getIdLong();
+
+            TempVoiceManager.verificarCallVazia(
+                    event.getGuild(),
+                    canalSaidoId
+            );
         }
     }
-
 
 }
--- src/main/java/org/javabot/commands/Managers/TempVoice.java
+++ src/main/java/org/javabot/commands/Managers/TempVoice.java
@@ -1,4 +1,30 @@
 package org.javabot.commands.Managers;
 
 public class TempVoice {
+
+    private final long voiceChannelId;
+    private final long ownerId;
+    private final long guildId;
+
+    public TempVoice(
+            long voiceChannelId,
+            long ownerId,
+            long guildId
+    ) {
+        this.voiceChannelId = voiceChannelId;
+        this.ownerId = ownerId;
+        this.guildId = guildId;
+    }
+
+    public long getVoiceChannelId() {
+        return voiceChannelId;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public long getGuildId() {
+        return guildId;
+    }
 }
